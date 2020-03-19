03d6066876198862291f60006ecdfb20a94fd366
lainshihenwy <henryle913@outlook.com>
2020-03-19 02:06:46 -0500

    Step 3: create a stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
@@ -6,17 +7,30 @@ void main() => runApp(
       ),
     );
 
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.asset('images/ball1.png'),
+    );
+  }
+}
+
 class BallPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blue,
       appBar: AppBar(
         title: Text('Ask Me Anything'),
         backgroundColor: Colors.blue,
       ),
-      body: Container(
-        color: Colors.blue,
-      ),
+      body: Ball(),
     );
   }
 }
